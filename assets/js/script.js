04d6db5dc3de5479a49a273480f3803683276f9b
Sakeeb Ahsan <sahsan97@terpmail.umd.edu>
2024-10-17 09:51:04 -0400

    changed homepage design

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.querySelectorAll('.slide');
+    if (n >= slides.length) { slideIndex = 0 }
+    if (n < 0) { slideIndex = slides.length - 1 }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
+
+// Automatically change slides every 3 seconds
+setInterval(function() {
+    plusSlides(1);
+}, 3000);
